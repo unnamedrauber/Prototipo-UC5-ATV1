48ecd442cdd7dae2510178a2ac2bea0fc859c555
unnamedrauber <77356181+unnamedrauber@users.noreply.github.com>
2021-06-27 13:47:48 -0300

    Add files via upload
    atv 2 novo

---
+++ Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject menuCanvas;
+    public GameObject creditCanvas;
+
+    private void Start() {
+        menuCanvas.SetActive(true);
+        creditCanvas.SetActive(false);
+    }
+
+    public void startGame() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void openCredits() {
+        menuCanvas.SetActive(false);
+        creditCanvas.SetActive(true);
+    }
+
+    public void backToMenu() {
+        menuCanvas.SetActive(true);
+        creditCanvas.SetActive(false);
+    }
+
+    public void exitGame() {
+        Application.Quit();
+    }
+}
